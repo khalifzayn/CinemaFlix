40d02ae7bbf16790993dd7e068fd1dc8066c1722
Faisal Zuhri <faisalzero.fz@gmail.com>
2024-09-11 22:10:14 +0700

    Base Project

---
+++ app/src/main/java/com/fszdev/cinemaflix/SplashScreenActivity.kt
@@ -0,0 +1,21 @@
+package com.fszdev.cinemaflix
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import com.fszdev.cinemaflix.ui.theme.CinemaFlixTheme
+
+@SuppressLint("CustomSplashScreen")
+class SplashScreenActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            CinemaFlixTheme {
+
+            }
+        }
+    }
+}
