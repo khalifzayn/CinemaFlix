fc37423b5189c8800d2ba167188d465a7c6c975f
Faisal Zuhri <faisalzero.fz@gmail.com>
2024-09-18 21:15:27 +0700

    Provide Icon and Adjust Splash Screen Section

--- feature/splash-screen/src/main/java/com/fszdev/splash_screen/screen/section/SplashSection.kt
+++ feature/splash-screen/src/main/java/com/fszdev/splash_screen/screen/section/SplashSection.kt
@@ -1,18 +1,36 @@
 package com.fszdev.splash_screen.screen.section
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Surface
 import androidx.compose.material3.surfaceColorAtElevation
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.fszdev.splash_screen.R
 
 @Composable
 internal fun SplashSection(modifier: Modifier = Modifier) {
-    Surface(
-        modifier = modifier.fillMaxSize(),
-        color = Color(0xFF01193C)
+    Box(
+        modifier = modifier
+            .fillMaxSize()
+            .background(color = Color(0xFF01193C)),
+        contentAlignment = Alignment.Center
     ){
-
+        Image(
+            painter = painterResource(id = R.drawable.icon_splash_screen ),
+            contentDescription = "splash screen logo",
+        )
     }
+}
+
+@Preview
+@Composable
+private fun SplashSectionReview() {
+    SplashSection()
 }
